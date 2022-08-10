e92eaa83303bbf154e8be9d5e86850077fd8cf89
Aaron Omale <Aaronarphaxad@gmail.com>
2022-08-11 00:27:56 +0100

    add recaptcha

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -50,7 +50,6 @@ export default function Contact() {
         </div>
         <form
           method="post"
-          data-netlify-recaptcha="true"
           // onSubmit={handleSubmit}
           name="contact"
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
