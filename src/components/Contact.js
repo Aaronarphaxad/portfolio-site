1d4813851493ee00f15b1d15c98edc7378318647
Aaron Omale <Aaronarphaxad@gmail.com>
2022-08-11 00:12:04 +0100

    match form name

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 export default function Contact() {
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [message, setMessage] = React.useState("");
-
   function encode(data) {
     return Object.keys(data)
       .map(
@@ -69,14 +65,12 @@ export default function Contact() {
           </div>
         </div>
         <form
-          data-netlify="true"
-          netlify
           method="post"
-          onSubmit={handleSubmit}
+          // onSubmit={handleSubmit}
           name="contact"
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
-          {/* <input type="hidden" name="form-name" value="contact" /> */}
+          <input type="hidden" name="form-name" value="contact" />
 
           <h2 className="dark:text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
@@ -97,8 +91,6 @@ export default function Contact() {
               id="name"
               name="name"
               required
-              value={name}
-              onChange={(e) => setName(e.target.value)}
               className="w-full bg-gray-200 focus:border-gray-800 dark:bg-gray-800 rounded border border-gray-700 dark:focus:border-indigo-500 focus:ring-2 dark:focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
@@ -114,8 +106,6 @@ export default function Contact() {
               id="email"
               name="email"
               required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
               className="w-full bg-gray-200 focus:border-gray-800 dark:bg-gray-800 rounded border border-gray-700 dark:focus:border-indigo-500 focus:ring-2 dark:focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
@@ -130,12 +120,9 @@ export default function Contact() {
               id="message"
               name="message"
               required
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
               className="w-full bg-gray-200 focus:border-gray-800 dark:bg-gray-800 rounded border border-gray-700 dark:focus:border-indigo-500 focus:ring-2 dark:focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
             />
           </div>
-          <div data-netlify-recaptcha="true"></div>
           <button
             type="submit"
             className="text-white bg-green-400 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
