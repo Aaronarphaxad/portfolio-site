40dc080af1896a69dd40bd25a1b97481e151f062
Aaron Omale <Aaronarphaxad@gmail.com>
2022-08-10 23:06:42 +0100

    match form name

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -76,7 +76,7 @@ export default function Contact() {
           name="contact"
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
-          <input type="hidden" name="form-name" value="contact" />
+          {/* <input type="hidden" name="form-name" value="contact" /> */}
 
           <h2 className="dark:text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
