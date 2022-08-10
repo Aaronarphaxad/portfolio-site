a7b8aaf404c99b9929c79fb5c68a222ae38f735c
Aaron Omale <Aaronarphaxad@gmail.com>
2022-08-10 17:10:00 +0100

    configure email receipt

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -69,12 +69,14 @@ export default function Contact() {
           </div>
         </div>
         <form
-          data-netlify="true"
-          action="POST"
-          onSubmit={handleSubmit}
+          // data-netlify="true"
+          // netlify
+          // method="POST"
+          // onSubmit={handleSubmit}
           name="contact"
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
+          <input type="hidden" name="form-name" value="contact" />
           <h2 className="dark:text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
           </h2>
@@ -93,6 +95,7 @@ export default function Contact() {
               type="text"
               id="name"
               name="name"
+              required
               className="w-full bg-gray-200 focus:border-gray-800 dark:bg-gray-800 rounded border border-gray-700 dark:focus:border-indigo-500 focus:ring-2 dark:focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
@@ -107,6 +110,7 @@ export default function Contact() {
               type="email"
               id="email"
               name="email"
+              required
               className="w-full bg-gray-200 focus:border-gray-800 dark:bg-gray-800 rounded border border-gray-700 dark:focus:border-indigo-500 focus:ring-2 dark:focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
@@ -120,6 +124,7 @@ export default function Contact() {
             <textarea
               id="message"
               name="message"
+              required
               className="w-full bg-gray-200 focus:border-gray-800 dark:bg-gray-800 rounded border border-gray-700 dark:focus:border-indigo-500 focus:ring-2 dark:focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
             />
           </div>
