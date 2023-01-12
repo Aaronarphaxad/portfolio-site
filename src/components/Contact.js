e1d385d21218eaf8f8583c2c22a3b8022bdc1838
Aaron Omale <Aaronarphaxad@gmail.com>
2023-01-12 12:15:15 -0800

    fix number

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -44,7 +44,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">(236)836-5191</p>
+              <p className="leading-relaxed">(236)863-5191</p>
             </div>
           </div>
         </div>
